838123c57f2f23de4abe6e8d474b20db02bd65d6
Blake <3397261+BlakeRxxk@users.noreply.github.com>
2026-06-11 12:10:33 +0200

    Update ProfileRow to use URL

--- modules/ruc-ui/Sources/RUCUI/Components/Profile/ProfileRowContent.swift
+++ modules/ruc-ui/Sources/RUCUI/Components/Profile/ProfileRowContent.swift
@@ -6,20 +6,20 @@
 import SwiftUI
 
 public struct ProfileRow: SwiftUI.View {
-    public init(name: String, subtitle: String? = nil, avatarImage: Image? = nil) {
+    public init(name: String, subtitle: String? = nil, avatarURL: URL? = nil) {
         self.name = name
         self.subtitle = subtitle
-        self.avatarImage = avatarImage
+        self.avatarURL = avatarURL
     }
 
     public var body: some SwiftUI.View {
         HStack(spacing: 16) {
-            AvatarView(image: avatarImage, name: name)
+            AvatarView(url: avatarURL, name: name)
             ProfileInfo(title: name, subtitle: subtitle)
         }
     }
 
     let name: String
     let subtitle: String?
-    let avatarImage: Image?
+    let avatarURL: URL?
 }
