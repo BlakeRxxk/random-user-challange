620d09e98e92dd00b3dac9e5efff55040b2e222b
Blake <3397261+BlakeRxxk@users.noreply.github.com>
2026-06-11 23:58:11 +0200

    chore: small refactoring

--- modules/ruc-app/Sources/UserDetail/Views/UserDetailSwiftUIView.swift
+++ modules/ruc-app/Sources/UserDetail/Views/UserDetailSwiftUIView.swift
@@ -13,18 +13,24 @@ public struct UserDetailSwiftUIView: SwiftUI.View {
     public var body: some SwiftUI.View {
         ScrollView {
             if let user = model.displayModel {
-                AvatarView(url: user.avatarURL, name: user.firstName)
-                HStack {
+                VStack(spacing: 16) {
+                    AvatarView(url: user.avatarURL, name: user.firstName)
+
                     Text("\(user.firstName) \(user.lastName)")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+
+                    Text(user.address)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 16)
+
+                    VStack(alignment: .leading) {
+                        SingleRow(title: "email:", content: user.email)
+                        SingleRow(title: "registration date:", content: user.registration)
+                        SingleRow(title: "gender:", content: user.gender)
+                    }
+                    .padding()
                 }
-                HStack {
-                    Text(user.address).multilineTextAlignment(.center)
-                }
-                VStack(alignment: .leading) {
-                    SingleRow(title: "email:", content: user.email)
-                    SingleRow(title: "registration date:", content: user.registration)
-                    SingleRow(title: "gender:", content: user.gender)
-                }.padding()
             }
         }.padding(.top, 80)
     }
